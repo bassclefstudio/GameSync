6c063baaf5623c1bbe638d8cdb7776e276b8cfb2
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-08-29 11:49:37 -0400

    Edited game save service (variance bug).

--- BassClefStudio.NET.GameSync/IGameSaveService.cs
+++ BassClefStudio.NET.GameSync/IGameSaveService.cs
@@ -4,8 +4,16 @@ using System.Text;
 
 namespace BassClefStudio.NET.GameSync
 {
-    public interface IGameSaveService<in T> where T : IGameState
+    /// <summary>
+    /// Represents a service that can make save copies of a <typeparamref name="T"/> game state.
+    /// </summary>
+    /// <typeparam name="T">The type of <see cref="IGameState"/> this service saves.</typeparam>
+    public interface IGameSaveService<T> where T : IGameState
     {
+        /// <summary>
+        /// Create a by-value save copy of the <typeparamref name="T"/> state.
+        /// </summary>
+        /// <param name="state">The existing <typeparamref name="T"/> state to copy.</param>
         T Copy(T state);
     }
 }
