ddf592784e25f346ed01efa348da20740dc50c98
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-27 14:56:20 -0400

    Added commit JSON converters and updated .Serialization package reference.

--- BassClefStudio.NET.GameSync/Files/CommitConverterService.cs
+++ BassClefStudio.NET.GameSync/Files/CommitConverterService.cs
@@ -7,6 +7,7 @@ using BassClefStudio.Serialization.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
--- BassClefStudio.NET.GameSync/Files/CommitJsonConverters.cs
+++ BassClefStudio.NET.GameSync/Files/CommitJsonConverters.cs
@@ -2,17 +2,49 @@
 using BassClefStudio.NET.GameSync.State;
 using BassClefStudio.Serialization;
 using BassClefStudio.Serialization.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace BassClefStudio.NET.GameSync.Files
 {
-    //public class CommitToJsonConverter<T> : IToJsonConverter<IGameCommit<T>> where T : GameState
-    //{
-    //}
+    /// <summary>
+    /// An <see cref="IToJsonConverter{T}"/> for seserializing <see cref="IGameCommit{T}"/>s.
+    /// </summary>
+    public class CommitToJsonConverter<T> : IToJsonConverter<IGameCommit<T>> where T : GameState
+    {
+        public IEnumerable<IToJsonConverter<IGameAction<T>>> ActionConverters { get; set; }
 
-    //public class CommitFromJsonConverter<T> : IFromJsonConverter<IGameCommit<T>> where T : GameState
-    //{
-    //}
+        /// <inheritdoc/>
+        public bool CanConvert(IGameCommit<T> item) => true;
+
+        /// <inheritdoc/>
+        public JToken Convert(IGameCommit<T> item)
+        {
+            return new JObject(
+                new JProperty("type", "commit"),
+                new JProperty("actions", item.GetActions().Select(a => ActionConverters.GetTo(a))));
+        }
+    }
+
+    /// <summary>
+    /// An <see cref="IFromJsonConverter{T}"/> for deserializing <see cref="IGameCommit{T}"/>s.
+    /// </summary>
+    public class CommitFromJsonConverter<T> : IFromJsonConverter<IGameCommit<T>> where T : GameState
+    {
+        public IEnumerable<IFromJsonConverter<IGameAction<T>>> ActionConverters { get; set; }
+
+        /// <inheritdoc/>
+        public bool CanConvert(JToken item) => item.IsJsonType("commit");
+
+        /// <inheritdoc/>
+        public IGameCommit<T> Convert(JToken item)
+        {
+            IEnumerable<IGameAction<T>> actions = item["actions"].Select(a => ActionConverters.GetTo(a));
+            return new GameCommit<T>(actions);
+        }
+    }
 }
