6ca5bbdcdf6ef285237a3fb7a56c79d6dafa2119
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-27 12:17:58 -0400

    Fixed breaking bugs in current build.

--- BassClefStudio.NET.GameSync/Files/CommitConverterService.cs
+++ BassClefStudio.NET.GameSync/Files/CommitConverterService.cs
@@ -12,16 +12,26 @@ using System.Text;
 
 namespace BassClefStudio.NET.GameSync.Files
 {
+    /// <summary>
+    /// Represents a service for converting <see cref="IGameCommit{T}"/> objects to and from JSON.
+    /// </summary>
+    /// <typeparam name="T">The generic parameter for the <see cref="IGameCommit{T}"/>s being converted.</typeparam>
     public class CommitConverterService<T> : ConverterService<IGameCommit<T>, JToken, JToken> where T : GameState
     {
+        /// <summary>
+        /// Creates a new <see cref="CommitConverterService{T}"/>.
+        /// </summary>
+        /// <param name="assemblies">A collection of <see cref="Assembly"/> objects used for retrieving the necessary <see cref="IConverter{TFrom, TTo}"/>s for (de)serialization.</param>
         public CommitConverterService(params Assembly[] assemblies) : base(assemblies)
         { }
 
+        /// <inheritdoc/>
         public override IGameCommit<T> ReadItem(JToken input)
         {
             return ConverterContainer.Resolve<IFromJsonConverter<IGameCommit<T>>>().GetTo(input);
         }
 
+        /// <inheritdoc/>
         public override JToken WriteItem(IGameCommit<T> item)
         {
             return ConverterContainer.Resolve<IToJsonConverter<IGameCommit<T>>>().GetTo(item);
