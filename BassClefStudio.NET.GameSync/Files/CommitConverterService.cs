60ad6bb4992d6e360012840c9b5998179bfa9f6f
bassclefstudio <51424093+bassclefstudio@users.noreply.github.com>
2020-09-27 12:13:16 -0400

    Added the JSON converters logic into the main project.

---
+++ BassClefStudio.NET.GameSync/Files/CommitConverterService.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using BassClefStudio.NET.GameSync.Commits;
+using BassClefStudio.NET.GameSync.State;
+using BassClefStudio.Serialization;
+using BassClefStudio.Serialization.DI;
+using BassClefStudio.Serialization.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace BassClefStudio.NET.GameSync.Files
+{
+    public class CommitConverterService<T> : ConverterService<IGameCommit<T>, JToken, JToken> where T : GameState
+    {
+        public CommitConverterService(params Assembly[] assemblies) : base(assemblies)
+        { }
+
+        public override IGameCommit<T> ReadItem(JToken input)
+        {
+            return ConverterContainer.Resolve<IFromJsonConverter<IGameCommit<T>>>().GetTo(input);
+        }
+
+        public override JToken WriteItem(IGameCommit<T> item)
+        {
+            return ConverterContainer.Resolve<IToJsonConverter<IGameCommit<T>>>().GetTo(item);
+        }
+    }
+}
---
+++ BassClefStudio.NET.GameSync/Files/CommitJsonConverters.cs
@@ -0,0 +1,13 @@
+﻿using BassClefStudio.NET.GameSync.Commits;
+using BassClefStudio.Serialization;
+using BassClefStudio.Serialization.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BassClefStudio.NET.GameSync.Files
+{
+    public class CommitToJsonConverter<T> : IToJsonConverter<IGameCommit<T>>
+    {
+    }
+}
